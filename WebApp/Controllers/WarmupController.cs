64022d3f9ae2257f6293b7e06fd0799a134a25ea
Giorgos Brantzos <g.brantzos@gmail.com>
2019-09-24 22:59:01 +0300

    Added WebApp

---
+++ WebApp/Controllers/WarmupController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class WarmupController : Controller
+    {
+        private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+        // GET: Warm up
+        public ActionResult Index()
+        {
+            Logger.Info("Welcome to warm up page...");
+            return View();
+        }
+    }
+}
