900bdd5d3f49f814a657e01333a7b9c0dc3b9e53
Giorgos Brantzos <g.brantzos@gmail.com>
2019-09-25 19:49:26 +0300

    Having party with Autofac and decorators

--- Decorators/Program.cs
+++ Decorators/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Autofac;
 using Autofac.Features.Decorators;
+using MediatR;
 
 namespace Decorators
 {
@@ -15,10 +18,38 @@ namespace Decorators
             builder.RegisterDecorator<LoggingService, IService>();
             builder.RegisterDecorator<TransactionalService, IService>();
 
+            builder
+              .RegisterType<Mediator>()
+              .As<IMediator>()
+              .InstancePerLifetimeScope();
+            builder.Register<ServiceFactory>(context =>
+            {
+                var c = context.Resolve<IComponentContext>();
+                return t => c.Resolve(t);
+            });
+
+            // Register handler
+            builder.RegisterType<SimpleRequestHandler>().AsImplementedInterfaces().InstancePerDependency();
+
+            // Register handler specific decorators
+            builder.RegisterDecorator<EnhancedSimpleRequestHandler, IRequestHandler<SimpleRequest, bool>>();
+
+            // Register generic decorators (will be the outer decorator, runs first and last!)
+            builder.RegisterGenericDecorator(typeof(GenericRequestHandlerDecorator<,>), typeof(IRequestHandler<,>));
+
             var container = builder.Build();
             var service = container.Resolve<IService>();
 
             service.Do("This is a message!");
+
+            var request = new SimpleRequest
+            {
+                ConnectionString = "there"
+            };
+            var mediatr = container.Resolve<IMediator>();
+            var result = mediatr.Send(request).Result;
+            Console.WriteLine($"Result was: '{result}'");
+
             Console.ReadLine();
         }
     }
@@ -60,6 +91,55 @@ namespace Decorators
             Console.WriteLine($"Inside {nameof(TransactionalService)}, before inner call.");
             this.innerService.Do(message);
             Console.WriteLine($"Inside {nameof(TransactionalService)}, after inner call.");
+        }
+    }
+
+    public class SimpleRequest : IRequest<bool>
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public class SimpleRequestHandler : IRequestHandler<SimpleRequest, bool>
+    {
+        public Task<bool> Handle(SimpleRequest request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Base request handler {nameof(SimpleRequestHandler)}");
+            return Task.FromResult(true);
+        }
+    }
+
+    public class EnhancedSimpleRequestHandler : IRequestHandler<SimpleRequest, bool>
+    {
+        private readonly IRequestHandler<SimpleRequest, bool> inner;
+
+        public EnhancedSimpleRequestHandler(IRequestHandler<SimpleRequest, bool> inner)
+        {
+            this.inner = inner;
+        }
+
+        public Task<bool> Handle(SimpleRequest request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Request - Response specific decorator {nameof(EnhancedSimpleRequestHandler)}");
+            return this.inner.Handle(request, cancellationToken);
+        }
+    }
+
+    public class GenericRequestHandlerDecorator<TRequest, TResponse> : IRequestHandler<TRequest, TResponse>
+        where TRequest : IRequest<TResponse>
+    {
+        private readonly IRequestHandler<TRequest, TResponse> inner;
+        private readonly IDecoratorContext context;
+
+        public GenericRequestHandlerDecorator(IRequestHandler<TRequest, TResponse> inner, IDecoratorContext context)
+        {
+            this.inner = inner;
+            this.context = context;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"Generic decorator {nameof(GenericRequestHandlerDecorator<TRequest, TResponse>)}");
+            return this.inner.Handle(request, cancellationToken);
         }
     }
 }
