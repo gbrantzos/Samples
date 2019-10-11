8c584c945308b04cec31d702dea0e3f65ae67d78
Giorgos Brantzos <g.brantzos@gmail.com>
2019-10-11 12:26:18 +0300

    Start console maximized when on DEBUG

--- QuartzHostedService/Program.cs
+++ QuartzHostedService/Program.cs
@@ -1,30 +1,70 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Web;
+
+#if DEBUG
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+#endif
 
 namespace QuartzHostedService
 {
     public static class Program
     {
+        #if DEBUG
+        [DllImport("user32.dll")]
+        public static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+        private const int HIDE     = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE  = 9;
+        #endif
+
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args)
-                .Build()
-                .Run();
+            #if DEBUG
+            Process p = Process.GetCurrentProcess();
+            ShowWindow(p.MainWindowHandle, MAXIMIZE);
+            #endif
+
+            var logger = GetLogger();
+            try
+            {
+                logger.Info("Starting application...");
+                CreateHostBuilder(args)
+                    .Build()
+                    .Run();
+                logger.Info("Application shutdown.");
+            }
+            catch (Exception ex)
+            {
+                // Catch setup errors
+                logger.Error(ex, "Stopped program because of exception!");
+                throw;
+            }
+            finally
+            {
+                // Shutdown logger
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-                })
+                .ConfigureWebHostDefaults(webBuilder => webBuilder.UseStartup<Startup>())
+                .ConfigureLogging(logging => logging.ClearProviders())
                 .UseNLog();
 
+        private static NLog.Logger GetLogger()
+        {
+            // Setup the logger first to catch all errors
+            var binPath    = Path.GetDirectoryName(typeof(Program).Assembly.Location);
+            var configPath = Path.Combine(binPath, "nlog.config");
+
+            return NLogBuilder.ConfigureNLog(configPath).GetCurrentClassLogger();
+        }
     }
 }
