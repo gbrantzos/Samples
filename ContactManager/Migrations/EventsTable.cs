c05838dcf411e4849f944ad2aff40b7b549a9f45
Giorgos Brantzos <g.brantzos@gmail.com>
2020-03-13 11:55:22 +0200

    Introducing FluentMigrator

---
+++ ContactManager/Migrations/EventsTable.cs
@@ -0,0 +1,21 @@
+﻿using FluentMigrator;
+
+namespace ContactManager.Migrations
+{
+    [Migration(20200312001, description: "Create Events table")]
+    public class EventsTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Events")
+                .WithColumn("EventID").AsGuid().NotNullable().PrimaryKey()
+                .WithColumn("AggregateID").AsInt64().NotNullable()
+                .WithColumn("Version").AsInt32().NotNullable()
+                .WithColumn("CreatedAt").AsDateTime2().NotNullable()
+                .WithColumn("EventType").AsString()
+                .WithColumn("Payload").AsString();
+        }
+
+        public override void Down() { }
+    }
+}
--- ContactManager/Program.cs
+++ ContactManager/Program.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using Autofac.Extensions.DependencyInjection;
 using ContactManager.API;
+using FluentMigrator;
+using FluentMigrator.Infrastructure;
+using FluentMigrator.Runner;
 using MediatR;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
@@ -19,6 +24,37 @@ namespace ContactManager
             try
             {
                 currentLogger = GetNLogProvider().CreateLogger(typeof(Program).FullName);
+
+                var serviceProvider = new ServiceCollection()
+                    .AddFluentMigratorCore()
+                    .ConfigureRunner(r =>
+                    {
+                        r.AddSqlServer();
+                        r.WithGlobalConnectionString("Server=(local);Database=DDDSample;Trusted_Connection=True;MultipleActiveResultSets=true");
+                        r.ScanIn(typeof(Program).Assembly);
+                    })
+                    .AddLogging(l => l.AddNLog())
+                    .BuildServiceProvider(false);
+                using(var scope = serviceProvider.CreateScope())
+                {
+                    var runner = scope.ServiceProvider.GetRequiredService<IMigrationRunner>();
+                    if (runner.HasMigrationsToApplyUp())
+                    {
+                        var context   = scope.ServiceProvider.GetRequiredService<IMigrationContext>();
+                        var generator = scope.ServiceProvider.GetRequiredService<IMigrationGenerator>();
+
+                        var info      = runner.MigrationLoader.LoadMigrations();
+                        var migration = info[20200312001].Migration;
+
+                        migration.GetUpExpressions(context);
+                        var exp = context.Expressions.FirstOrDefault();
+
+                        string script = generator.Generate((dynamic)exp);
+                        currentLogger.LogWarning(script);
+                        // runner.MigrateUp();
+                    }
+                };
+
                 currentLogger.LogInformation("Contact Manager starting...");
                 CreateHostBuilder(args)
                     .Build()
