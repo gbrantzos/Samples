a7d6bd64ab875682d2ee1415b6d053ebd58b0b58
Giorgos Brantzos <g.brantzos@gmail.com>
2020-03-19 16:56:00 +0200

    Add converter to Config data

---
+++ Workbench/ConfigurationExtensions.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Workbench
+{
+    public static class ConfigurationExtensions
+    {
+        public static Dictionary<string,string> ToConfigData(this object source, string root = null)
+        {
+            var result = new Dictionary<string, string>();
+            ConvertInternal(result, source, root ?? String.Empty);
+
+            return result;
+        }
+
+        private static void ConvertInternal(Dictionary<string, string> result, object source, string rootKey)
+        {
+            var properties = source.GetType().GetProperties();
+            foreach (var prop in properties)
+            {
+                var keyName = String.IsNullOrEmpty(rootKey) ? prop.Name : rootKey + ':' + prop.Name;
+                var value = prop.GetValue(source, null);
+
+                if (value == null) return;
+
+                if (prop.PropertyType.IsPrimitive || prop.PropertyType == typeof(string))
+                {
+                    result[keyName] = value.ToString();
+                }
+                else if (prop.PropertyType == typeof(DateTime))
+                {
+                    result[keyName] = ((DateTime)value).ToString("s");
+                }
+                else if (value is IEnumerable enumerableValue)
+                {
+                    var index = 0;
+                    foreach(object child in enumerableValue)
+                    {
+                        ConvertInternal(result, child, $"{keyName}:{index}");
+                        index++;
+                    }
+                }
+                else
+                {
+                    ConvertInternal(result, value, keyName);
+                }
+            }
+        }
+    }
+}
--- Workbench/Program.cs
+++ Workbench/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using Workbench;
 
 namespace Sandbox
 {
@@ -6,8 +9,70 @@ namespace Sandbox
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+            var agreement = new Agreement
+            {
+                ID = 101,
+                Description = "Convert to dictionary",
+                DateOfBirth = new DateTime(1976, 6, 3),
+                Customer = new Customer
+                {
+                    FirstName = "John",
+                    LastName = "Smith",
+                    Age = 30,
+                    Address = new List<Address>()
+                    {
+                        new Address
+                        {
+                            Name = "Los Angeles 1",
+                            ZipCode = 25437
+                        },
+                        new Address
+                        {
+                            Name = "New York 25",
+                            ZipCode = 25441
+                        }
+                    }
+                }
+            };
+
+            var result = agreement.ToConfigData(nameof(Agreement));
+            Console.WriteLine("Agreement");
+            Console.WriteLine(new String('=', 80));
+            Console.WriteLine();
+
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} = {item.Value}");
+            }
+
+            var config = new ConfigurationBuilder()
+                .AddInMemoryCollection(result)
+                .Build();
+            var fromConfig = config.GetSection(nameof(Agreement)).Get<Agreement>();
+
             Console.ReadLine();
         }
+    }
+
+    internal class Agreement
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public Customer Customer { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
+    internal class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public IEnumerable<Address> Address { get; set; }
+    }
+
+    internal class Address
+    {
+        public string Name { get; set; }
+        public int ZipCode { get; set; }
     }
 }
